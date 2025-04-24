8d318641344bfd29a90726adcb15504f2ff2e8e1
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-24 15:46:41 +0200

    refactoring

--- src/main/scala/Matrix.scala
+++ src/main/scala/Matrix.scala
@@ -1,5 +1,4 @@
 case class Matrix(s: String):
-   private val m: Vector[Vector[Int]] = s.split("\n").map(_.split(" ").map(_.toInt).toVector).toVector
-
-   def row(r: Int): Vector[Int] = m(r)
-   def column(c: Int): Vector[Int] = m.map(_(c))
+   private val m = s.split("\n").map(_.split(" ").map(_.toInt).toVector).toVector
+   def row(r: Int): Seq[Int] = m(r)
+   def column(c: Int): Seq[Int] = m.map(_(c))
